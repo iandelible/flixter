d5109bc6e1d4a8713736c0c3b2be59822ee2f271
Ian Pan <ianpan@hotmail.com>
2016-10-27 20:44:14 +0000

    student enrollment

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'static_pages#index'
-  resources :courses, only:[:index, :show]
+  resources :courses, only:[:index, :show] do
+    resources :enrollments, only: :create
+  end
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :sections, only: [] do
