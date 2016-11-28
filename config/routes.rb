1a3909d22ea684672730dfa79c0db5e15c5865d7
Ian Pan <ianpan@hotmail.com>
2016-11-28 23:08:19 +0000

    instructor dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  resource :dashboard, only: [:show]
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'static_pages#index'
